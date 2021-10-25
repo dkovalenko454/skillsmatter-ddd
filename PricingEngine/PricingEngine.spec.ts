b5c17a37c0cfe7d21c1f329f65de84b8554d0bd1
Dmitri Kovalenko <dmitri.kovalenko@everylifetechnologies.com>
2021-10-25 19:30:19 +0100

    Extra mileage tests for the pricing engine

--- PricingEngine/PricingEngine.spec.ts
+++ PricingEngine/PricingEngine.spec.ts
@@ -1,5 +1,5 @@
 import {Expect, Test, TestCase} from 'alsatian';
-import {Duration, DurationInMinutes, pricingEngine} from "./PricingEngine";
+import {Duration, DurationInMinutes, Mileage, MileageInKilometres, pricingEngine} from "./PricingEngine";
 import {EUR, Money} from "../Money/Money";
 
 export class PricingEngineSpec {
@@ -15,12 +15,32 @@ export class PricingEngineSpec {
     }
 
     @Test()
+    // no free minutes - default
     @TestCase(EUR(30), DurationInMinutes(3), DurationInMinutes(0), EUR(90))
+    // duration > free minutes
     @TestCase(EUR(30), DurationInMinutes(20), DurationInMinutes(10), EUR(300))
+    // duration < free minutes
     @TestCase(EUR(30), DurationInMinutes(5), DurationInMinutes(10), EUR(0))
+    // duration == free minutes
     @TestCase(EUR(30), DurationInMinutes(10), DurationInMinutes(10), EUR(0))
     CalculatePrice_extended_reservation_charged_per_minute(pricePerMinute: Money, duration: Duration, freeMinutes: Duration, totalPrice: Money) {
         const actual = pricingEngine(pricePerMinute, duration, freeMinutes);
+        const expected = totalPrice;
+
+        Expect(actual.equalTo(expected)).toBe(true);
+    }
+
+    @Test()
+    // no free mileage - default
+    @TestCase(EUR(10), MileageInKilometres(120), MileageInKilometres(0), EUR(1200))
+    // mileage > free mileage
+    @TestCase(EUR(10), MileageInKilometres(120), MileageInKilometres(100), EUR(200))
+    // mileage < free mileage
+    @TestCase(EUR(10), MileageInKilometres(60), MileageInKilometres(100), EUR(0))
+    // mileage == free mileage
+    @TestCase(EUR(10), MileageInKilometres(100), MileageInKilometres(100), EUR(0))
+    CalculatePrice_extra_mileage_charged_per_km(pricePerKm: Money, mileage: Mileage, freeMileage: Mileage, totalPrice: Money) {
+        const actual = pricingEngine(pricePerKm, mileage, freeMileage);
         const expected = totalPrice;
 
         Expect(actual.equalTo(expected)).toBe(true);
--- PricingEngine/PricingEngine.ts
+++ PricingEngine/PricingEngine.ts
@@ -1,10 +1,15 @@
 import {Money} from "../Money/Money";
 
 export type Duration = number;
+export type Mileage = number;
 export type CalculatePrice = (pricePerMinute: Money, durationInMinutes: Duration, freeMinutes?: Duration) => Money;
 
 export const DurationInMinutes = (duration: number): Duration => {
     return duration;
+}
+
+export const MileageInKilometres = (mileage: number): Mileage => {
+    return mileage;
 }
 
 export const pricingEngine: CalculatePrice = (pricePerMinute, durationInMinutes, freeMinutes: Duration = DurationInMinutes(0)) => {
